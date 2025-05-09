e5796abff6e234036f7a2667c7431bb939b8ad97
mahaboob-basha-a <alurumahaboobbasha@gmail.com>
2025-05-09 08:29:54 +0530

    resolved minor issues

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -35,11 +35,11 @@ const Orders = () => {
                         <div className="flex gap-5 max-w-80">
                             <img className="w-12 h-12 object-cover" src={assets.box_icon} alt="boxIcon" />
                             <div>
-                                {order.items.map((item, index) => (
+                                {order?.items?.map((item, index) => (
                                     <div key={index} className="flex flex-col">
                                         <p className="font-medium">
-                                            {item.product.name} 
-                                            <span className={`text-primary`}> x {item.quantity}</span>
+                                            {item?.product?.name} 
+                                            <span className={`text-primary`}> x {item?.quantity}</span>
                                         </p>
                                     </div>
                                 ))}
@@ -48,21 +48,21 @@ const Orders = () => {
 
                         <div className="text-sm md:text-base text-black/60">
                             <p className='text-black/80'>
-                            {order.address.firstName} {order.address.lastName}</p>
+                            {order?.address?.firstName} {order?.address?.lastName}</p>
 
-                            <p>{order.address.street}, {order.address.city}</p>
+                            <p>{order?.address?.street}, {order?.address?.city}</p>
 
-                            <p>{order.address.state},{order.address.zipcode}, {order.address.country}</p>
+                            <p>{order?.address?.state},{order?.address?.zipcode}, {order?.address?.country}</p>
                             <p></p>
-                            <p>{order.address.phone}</p>
+                            <p>{order?.address?.phone}</p>
                         </div>
 
-                        <p className="font-medium text-lg my-auto">{currency}{order.amount}</p>
+                        <p className="font-medium text-lg my-auto">{currency}{order?.amount}</p>
 
                         <div className="flex flex-col text-sm">
-                            <p>Method: {order.paymentType}</p>
-                            <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
-                            <p>Payment: {order.isPaid ? "Paid" : "Pending"}</p>
+                            <p>Method: {order?.paymentType}</p>
+                            <p>Date: {new Date(order?.createdAt).toLocaleDateString()}</p>
+                            <p>Payment: {order?.isPaid ? "Paid" : "Pending"}</p>
                         </div>
                     </div>
                 ))}
